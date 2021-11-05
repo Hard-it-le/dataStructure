7bbe9fbe6ca44cb15a84bb66a7c8c8e00a778e27
yujiale123 <yu18371919563@163.com>
2021-11-05 11:29:07 +0800

    1。leetcode之数组之寻找重复数之快慢指针

--- 00-leetcode/src/数组/_287_寻找重复数.java
+++ 00-leetcode/src/数组/_287_寻找重复数.java
@@ -73,6 +73,7 @@ public class _287_寻找重复数 {
      * 方法1：使用hashmap保存每个数
      * 时间复杂度O(n）
      * 空间复杂度 O(n)
+     * 不符合进阶要求
      *
      * @param nums
      * @return
@@ -93,6 +94,7 @@ public class _287_寻找重复数 {
      * 方法二、使用hashset保存数据，判断是否出现过
      * *  时间复杂度O(n）
      * *  空间复杂度 O(n)
+     * 不符合进阶要求
      *
      * @param nums
      * @return
@@ -112,6 +114,7 @@ public class _287_寻找重复数 {
      * 方法三：先排序，相邻元素的相同元素
      * 时间复杂度 O(log n)
      * 空间复杂度 O(1)
+     * 同时不符合进阶要求，改变了nums数组
      *
      * @param nums
      * @return
@@ -139,19 +142,16 @@ public class _287_寻找重复数 {
         // 定义左右指针
         int left = 1;
         int right = nums.length - 1;
-
         while (left <= right) {
             // 计算中间值
             int mid = (left + right) / 2;
-
             // 对当前的mid计算count值
             int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] <= mid) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] <= mid) {
                     count++;
                 }
             }
-
             // 判断count和mid本身的大小关系
             if (count <= mid) {
                 // count小于等于mid自身，说明mid比target小，左指针右移
@@ -159,7 +159,6 @@ public class _287_寻找重复数 {
             } else {
                 right = mid;
             }
-
             // 左右指针重合时，找到target
             if (left == right) {
                 return left;
@@ -170,16 +169,35 @@ public class _287_寻找重复数 {
 
     /**
      * 方法五、快慢指针
+     *
      * @param nums
      * @return
      */
     public int findDuplicate05(int[] nums) {
+        // 定义快慢指针
+        int fast = 0, slow = 0;
+        // 1. 寻找环内的相遇点
+        do {
+            // 快指针一次走两步，慢指针一次走一步
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while (fast != slow);
 
-        return -1;
+        // 循环结束，slow和fast相等，都是相遇点
+        // 2. 寻找环的入口点
+        // 另外定义两个指针，固定间距
+        int before = 0, after = slow;
+        while (before != after){
+            before = nums[before];
+            after = nums[after];
+        }
+
+        // 循环结束，相遇点就是环的入口点，也就是重复元素
+        return before;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5,4};
+        int[] arr = {1, 2, 3, 4, 5, 4};
         _287_寻找重复数 v = new _287_寻找重复数();
         int duplicate01 = v.findDuplicate04(arr);
         System.out.println(duplicate01);
