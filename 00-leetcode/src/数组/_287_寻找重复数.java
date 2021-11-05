d139781310199f467445de65ad37aefd00f12c37
yujiale123 <yu18371919563@163.com>
2021-11-05 09:48:21 +0800

    1。leetcode之数组之合并有序数组

---
+++ 00-leetcode/src/数组/_287_寻找重复数.java
@@ -0,0 +1,187 @@
+package 数组;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @program: data-structure
+ * @author: yjl
+ * @created: 2021/11/04
+ */
+//给定一个包含 n + 1 个整数的数组 nums ，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。
+//
+// 假设 nums 只有 一个重复的整数 ，找出 这个重复的数 。
+//
+// 你设计的解决方案必须不修改数组 nums 且只用常量级 O(1) 的额外空间。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,3,4,2,2]
+//输出：2
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [3,1,3,4,2]
+//输出：3
+//
+//
+// 示例 3：
+//
+//
+//输入：nums = [1,1]
+//输出：1
+//
+//
+// 示例 4：
+//
+//
+//输入：nums = [1,1,2]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 10⁵
+// nums.length == n + 1
+// 1 <= nums[i] <= n
+// nums 中 只有一个整数 出现 两次或多次 ，其余整数均只出现 一次
+//
+//
+//
+//
+// 进阶：
+//
+//
+// 如何证明 nums 中至少存在一个重复的数字?
+// 你可以设计一个线性级时间复杂度 O(n) 的解决方案吗？
+//
+// Related Topics 位运算 数组 双指针 二分查找 👍 1451 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class _287_寻找重复数 {
+    /**
+     * 方法1：使用hashmap保存每个数
+     * 时间复杂度O(n）
+     * 空间复杂度 O(n)
+     *
+     * @param nums
+     * @return
+     */
+    public int findDuplicate01(int[] nums) {
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for (Integer num : nums) {
+            //判断当前num是否在key中存在
+            if (countMap.containsKey(num)) {
+                return num;
+            }
+            countMap.put(num, 1);
+        }
+        return -1;
+    }
+
+    /**
+     * 方法二、使用hashset保存数据，判断是否出现过
+     * *  时间复杂度O(n）
+     * *  空间复杂度 O(n)
+     *
+     * @param nums
+     * @return
+     */
+    public int findDuplicate02(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int num : nums) {
+            if (hashSet.contains(num)) {
+                return num;
+            }
+            hashSet.add(num);
+        }
+        return -1;
+    }
+
+    /**
+     * 方法三：先排序，相邻元素的相同元素
+     * 时间复杂度 O(log n)
+     * 空间复杂度 O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public int findDuplicate03(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 方法四、二分查找，查找1~N的自然数序列，寻找target
+     * <p>
+     * 时间复杂度 O(nlog n))
+     * 空间复杂度 O（1）
+     *
+     * @param nums
+     * @return
+     */
+    public int findDuplicate04(int[] nums) {
+        // 定义左右指针
+        int left = 1;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            // 计算中间值
+            int mid = (left + right) / 2;
+
+            // 对当前的mid计算count值
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] <= mid) {
+                    count++;
+                }
+            }
+
+            // 判断count和mid本身的大小关系
+            if (count <= mid) {
+                // count小于等于mid自身，说明mid比target小，左指针右移
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+
+            // 左右指针重合时，找到target
+            if (left == right) {
+                return left;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 方法五、快慢指针
+     * @param nums
+     * @return
+     */
+    public int findDuplicate05(int[] nums) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5,4};
+        _287_寻找重复数 v = new _287_寻找重复数();
+        int duplicate01 = v.findDuplicate04(arr);
+        System.out.println(duplicate01);
+    }
+}
--- 00-leetcode/src/数组/_88_合并两个有序数组.java
+++ 00-leetcode/src/数组/_88_合并两个有序数组.java
@@ -11,6 +11,7 @@ public class _88_合并两个有序数组 {
     /**
      * 方法一，直接循环排序
      * 时间复杂度为O（n）
+     *
      * @param nums1
      * @param m
      * @param nums2
@@ -21,16 +22,48 @@ public class _88_合并两个有序数组 {
             nums1[m + i] = nums2[i];
         }
         Arrays.sort(nums1);
+
+
+    }
+
+    /**
+     * 方法二，直接保存
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge02(int[] nums1, int m, int[] nums2, int n) {
+        int n1 = n - 1;
+        for (int i = 0; i <= n1; i++) {
+            nums1[i + m] = nums2[i];
+        }
+        Arrays.sort(nums1);
+
+    }
+
+    /**
+     * 方法三：双指针法
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge03(int[] nums1, int m, int[] nums2, int n) {
+
+
     }
 
     public static void main(String[] args) {
         _88_合并两个有序数组 v = new _88_合并两个有序数组();
         int[] num1 = {1, 2, 3, 4, 0, 0, 0};
-        int n1=num1.length;
+        int n1 = num1.length;
         int[] num2 = {9, 1, 6};
         int n2 = num2.length;
         int i1 = n1 - n2;
-        v.merge01(num1, i1, num2, n2);
+        v.merge03(num1, i1, num2, n2);
         System.out.println(Arrays.toString(num1));
     }
 }
