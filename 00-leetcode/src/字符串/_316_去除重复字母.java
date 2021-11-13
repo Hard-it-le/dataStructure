33e5889419bd1c14d242b95a2d6db6d6e1946d7b
yujiale123 <yu18371919563@163.com>
2021-11-13 08:51:32 +0800

    1。leetcode之字符串之去除重复字母

---
+++ 00-leetcode/src/字符串/_316_去除重复字母.java
@@ -0,0 +1,49 @@
+package 字符串;
+
+/**
+ * @program: data-structure
+ * @author: yjl
+ * @created: 2021/11/12
+ */
+//给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+//
+// 注意：该题与 1081 https://leetcode-cn.com/problems/smallest-subsequence-of-
+//distinct-characters 相同
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "bcabc"
+//输出："abc"
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "cbacdcbc"
+//输出："acdb"
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 10⁴
+// s 由小写英文字母组成
+//
+// Related Topics 栈 贪心 字符串 单调栈 👍 613 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class _316_去除重复字母 {
+
+    public String removeDuplicateLetters(String s) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
