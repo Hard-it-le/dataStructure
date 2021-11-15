5352083f0b79e4b86550de8dd525cf7a90b6ce7b
yujiale123 <yu18371919563@163.com>
2021-11-15 22:02:33 +0800

    1。算法之冒泡排序

---
+++ 117-排序/src/BubbleSort.java
@@ -0,0 +1,34 @@
+/**
+ * @program: data-structure
+ * @author: yjl
+ * @created: 2021/11/14
+ * 冒泡排序
+ */
+class BubbleSort {
+
+    public static void bubbleSort01(int[] array) {
+        int length = array.length;
+
+        for (int j = length - 1; j > 0; j--) {
+            for (int i = 1; i <= j; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                }
+            }
+        }
+
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] array = {10, 6, 9, 12, 45, 67, 14, 32};
+        bubbleSort01(array);
+    }
+}
