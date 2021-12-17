5940c9fb5485a1b775fd6d2704ce97bdce808883
yujiale <yu18371919563@163.com>
2021-12-17 09:07:54 +0800

    leetCode之二分查找之852山脉数组

--- 00-leetcode/src/数组/_852_山脉数组的峰顶索引.java
+++ 00-leetcode/src/数组/_852_山脉数组的峰顶索引.java
@@ -76,16 +76,59 @@ package 数组;
 public class _852_山脉数组的峰顶索引 {
 
     public static void main(String[] args) {
+        int[] array = new int[]{2, 3, 5, 6, 9, 2};
 
+        _852_山脉数组的峰顶索引 v = new _852_山脉数组的峰顶索引();
+        int i = v.peakIndexInMountainArray(array);
+        System.out.println(i);
     }
 
 
     /**
+     * 局部最大值
+     *
+     * 山脉数组的峰顶索引
      *
      * @param arr
      * @return
      */
     public int peakIndexInMountainArray(int[] arr) {
+        int l = 1;
+        int r=arr.length -2;
+        int ans = 0;
+        while(l <= r ){
+            int  mid = l +((r - l)>> 1);
+            if(arr[mid]>arr[mid+1]){
+                ans = mid;
+                r=mid-1;
+            }else {
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
 
+
+    /**
+     * 局部最小
+     *
+     * @param arr
+     * @return
+     */
+    public int peakIndexInMountainArray02(int[] arr) {
+        int l = 1;
+        int r = arr.length - 2;
+        int mid = 0;
+        while (l < r) {
+            mid = l + ((r - l) >> 1);
+            if (arr[mid] > arr[mid - 1]) {
+                r = mid - 1;
+            } else if (arr[mid] > arr[mid + 1]) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return mid;
     }
 }
