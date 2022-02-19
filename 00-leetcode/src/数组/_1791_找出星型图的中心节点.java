246a943a5bd0831f2164783fb081678b7f79a561
yujiale <yu18371919563@163.com>
2022-02-19 17:14:43 +0800

    leetcode之1791找出星星图的中心节点

---
+++ 00-leetcode/src/数组/_1791_找出星型图的中心节点.java
@@ -0,0 +1,74 @@
+package 数组;
+
+//有一个无向的 星型 图，由 n 个编号从 1 到 n 的节点组成。星型图有一个 中心 节点，并且恰有 n - 1 条边将中心节点与其他每个节点连接起来。
+//
+// 给你一个二维整数数组 edges ，其中 edges[i] = [ui, vi] 表示在节点 ui 和 vi 之间存在一条边。请你找出并返回 edges
+//所表示星型图的中心节点。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：edges = [[1,2],[2,3],[4,2]]
+//输出：2
+//解释：如上图所示，节点 2 与其他每个节点都相连，所以节点 2 是中心节点。
+//
+//
+// 示例 2：
+//
+//
+//输入：edges = [[1,2],[5,1],[1,3],[1,4]]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 3 <= n <= 10⁵
+// edges.length == n - 1
+// edges[i].length == 2
+// 1 <= ui, vi <= n
+// ui != vi
+// 题目数据给出的 edges 表示一个有效的星型图
+//
+// Related Topics 图 👍 47 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * @program: dataStructure
+ * @author: yjl
+ * @created: 2022/02/19
+ */
+public class _1791_找出星型图的中心节点 {
+
+    public static void main(String[] args) {
+        _1791_找出星型图的中心节点 v = new _1791_找出星型图的中心节点();
+
+        int[][] ints = new int[][]{
+                {1, 2}, {2, 3}, {4, 2}
+        };
+        int center = v.findCenter(ints);
+        System.out.println(center);
+    }
+
+    /**
+     * 去除第一个数的数字，然后依次对比第二个数的两个数字，然后相等就返回那个
+     * @param edges
+     * @return
+     */
+    public int findCenter(int[][] edges) {
+        int a = edges[0][0];
+        int b = edges[0][1];
+        if (a == edges[1][0] || a == edges[1][1]) {
+            return a;
+        } else {
+            return b;
+        }
+
+    }
+}
